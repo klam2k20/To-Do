3756705440742f2698dda7f59c55d9f482c01b55
klam2k20 <klam2k17@gmail.com>
2023-01-13 01:32:55 -0500

    Add redux store

--- src/index.js
+++ src/index.js
@@ -6,16 +6,15 @@ import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import store from './app/store';
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
